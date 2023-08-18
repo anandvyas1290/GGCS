b03e6948a66e7f4288a3e09bb104470fa35d21f5
vinod_mestha <vinodmestha399@gmail.com>
2023-08-19 02:59:21 +0530

    footer design done

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../container/Footer'
+import CompanyBlog from '../container/CompanyBlog'
+
+function Routes() {
+    return (
+        <>
+            <CompanyBlog />
+            <Footer />
+        </>
+    )
+}
+
+export default Routes
