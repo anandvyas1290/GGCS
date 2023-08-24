ae48ec14abda8ba27dbf80695334f979535fc833
anand-vyas-gg <anand.vyas@globalgarner.com>
2023-08-24 18:23:03 +0530

    working on blog page

---
+++ src/routes/MainRoutes/Blog/index.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import blog1 from "../../../assets/blog/blog1.webp";
+import blog2 from "../../../assets/blog/blog2.webp";
+import blog3 from "../../../assets/blog/blog3.webp";
+import blog4 from "../../../assets/blog/blog4.webp";
+import blog5 from "../../../assets/blog/blog5.webp";
+import blog6 from "../../../assets/blog/blog6.webp";
+import blog7 from "../../../assets/blog/blog7.webp";
+
+const blogData = [
+    {
+        id: 1,
+        category: "Technical Seo",
+        label: "10 Quick Tips About Blogging",
+        date: "Oct 16,2023",
+        writer: "Eleanor Fant",
+        image: blog1,
+    },
+    {
+        id: 2,
+        category: "Technical Seo",
+        label: "10 Quick Tips About Blogging",
+        date: "Oct 16,2023",
+        writer: "Eleanor Fant",
+        image: blog2,
+    },
+    {
+        id: 3,
+        category: "Technical Seo",
+        label: "10 Quick Tips About Blogging",
+        date: "Oct 16,2023",
+        writer: "Eleanor Fant",
+        image: blog3,
+    },
+    {
+        id: 4,
+        category: "Technical Seo",
+        label: "10 Quick Tips About Blogging",
+        date: "Oct 16,2023",
+        writer: "Eleanor Fant",
+        image: blog1,
+    },
+    {
+        id: 5,
+        category: "Technical Seo",
+        label: "10 Quick Tips About Blogging",
+        date: "Oct 16,2023",
+        writer: "Eleanor Fant",
+        image: blog4,
+    },
+    {
+        id: 6,
+        category: "Technical Seo",
+        label: "10 Quick Tips About Blogging",
+        date: "Oct 16,2023",
+        writer: "Eleanor Fant",
+        image: blog5,
+    },
+    {
+        id: 7,
+        category: "Technical Seo",
+        label: "10 Quick Tips About Blogging",
+        date: "Oct 16,2023",
+        writer: "Eleanor Fant",
+        image: blog6,
+    },
+    {
+        id: 8,
+        category: "Technical Seo",
+        label: "10 Quick Tips About Blogging",
+        date: "Oct 16,2023",
+        writer: "Eleanor Fant",
+        image: blog7,
+    },
+];
+
+export default function Blog(props) {
+    return <div>Blog</div>;
+}
--- src/routes/MainRoutes/index.jsx
+++ src/routes/MainRoutes/index.jsx
@@ -3,11 +3,13 @@ import { useRoutes } from "react-router-dom";
 
 import Home from "./Home";
 import Services from "./Services";
+import Blog from "./Blog";
 
 export default function MainRoutes(props) {
     const routes = useRoutes([
         { path: "/", element: <Home {...props} /> },
         { path: "/services", element: <Services {...props} /> },
+        { path: "/blog", element: <Blog {...props} /> },
     ]);
     return routes;
 }
